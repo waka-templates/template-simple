cde116c25ee0bc94250ed6706fbd0eeb01575bf6
dwqs <461147874@qq.com>
2017-01-13 10:53:18 +0800

    prompt for test

--- meta.js
+++ meta.js
@@ -9,6 +9,10 @@ module.exports = {
             "required": true,
             "message" : "Project name"
         },
+        "version": {
+           "type": "input",
+            "message": "project's version"
+        },
         "description": {
             "type"    : "string",
             "required": false,
@@ -19,13 +23,42 @@ module.exports = {
             "type"   : "string",
             "message": "Author"
         },
-        "test": {
+        "unit": {
             "type": "confirm",
-            "message": "Need test?"
+            "message": "Setup unit tests with Mocha?"
+        },
+        "build": {
+            "type": "list",
+            "message": "Vue build",
+            "choices": [
+                {
+                    "name": "Runtime + Compiler: recommended for most users",
+                    "value": "standalone",
+                    "short": "standalone"
+                },
+                {
+                    "name": "Runtime-only: about 6KB lighter min+gzip, but templates (or any Vue-specific HTML) are ONLY allowed in .vue files - render functions are required elsewhere",
+                    "value": "runtime",
+                    "short": "runtime"
+                }
+            ]
+        },
+        "lint": {
+            "type": "confirm",
+            "message": "Use ESLint to lint your code?"
+        },
+        "webpack":{
+            "type": "confirm",
+            "message": "Use webpack 2?"
+        },
+        "suit":{
+            "type": "checkbox",
+            "message":"select suit?",
+            "default": ["vuex","vue-router","vue-resource"]
         }
     },
     "filters":{
         "test/*": "test"
     },
-    "completeMessage": "To get started:\n\n  cd {{destDirName}}\n  npm install\n  npm run dev\n\nDocumentation can be found at https://github.com/waka-templates/vue-webpack"
+    "completeMessage": "To get started:\n\n  cd {{destDirName}}\n  npm install\n  npm run dev\n\nDocumentation can be found at https://github.com/waka-templates/template-simple"
 }
